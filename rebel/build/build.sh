738a4f30237511805b4b00b3c80c1ff1d7400924
Andreas Venizelou <40325122+veniware@users.noreply.github.com>
2021-09-06 21:45:49 +0300

    Update build.sh

--- rebel/build/build.sh
+++ rebel/build/build.sh
@@ -1,3 +1,41 @@
-#!/bin/bash
+ #!/bin/bash
 
-#TODO:
+build=0
+if [ -e ./.buildcount ]; then
+    ((build=`cat ./.buildcount`+1))    
+    echo $build > ./.buildcount
+fi
+
+export GOOS='linux'
+export GOARCH='amd64'
+filename="${appname}-linux-amd64"
+
+appname="rebel"
+version="1.0.${build}"
+doRun=false
+isRelease=false
+
+for var in "$@"; do
+    if [ "$var" = "--run" ]; then
+        doRun=true
+    fi
+    if [ "$var" = "--release" ]; then
+        isRelease=true
+    fi
+done
+
+if [ $isRelease = true ]; then
+    go build \
+    -o ./$filename \
+    -ldflags "-X main.product_release=true -X main.product_version=${version} -s -w" \
+    ./..
+else
+    go build \
+    -o ./$filename \
+    -ldflags "-X main.product_release=true -X main.product_version=${version}" \
+    ./..
+fi
+
+if [ $doRun = true ]; then
+    ./rebel-linux-amd64
+fi
